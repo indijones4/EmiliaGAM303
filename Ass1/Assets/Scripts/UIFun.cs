c44061139a8f969eb44490ca517a599889fbf348
indijones4 <indijones4@gmail.com>
2018-02-22 14:54:52 +1100

    added package

---
+++ Ass1/Assets/Scripts/UIFun.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFun : MonoBehaviour {
+
+	public Text CountdownTimer;
+	public Text playerInput;
+	float Countdownfrom = 20f;
+	float Counter;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Counter = Countdownfrom;
+		CountdownTimer.text = Counter.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Counter -= Time.deltaTime;
+		if (Counter > 0) 
+		{
+			CountdownTimer.text = Counter.ToString ("##.##");
+		} 
+		else 
+		{
+			CountdownTimer.color = Color.red;
+			CountdownTimer.text = ("00.00");
+		}
+	}
+}
