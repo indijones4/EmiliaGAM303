09e213aedbca2864524cdf22e911e87852df64b6
indijones4 <indijones4@gmail.com>
2018-02-22 15:57:45 +1100

    Fun with Cars

---
+++ Ass1/Assets/Scripts/WayPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour 
+{	
+	WayPointHolder waypointHolder;
+
+	void Start()
+	{
+		waypointHolder = GameObject.Find ("WaypointHolder").GetComponent<WayPointHolder>();
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.name == "ColliderBottom") 
+		{
+			waypointHolder.UpdateScore ();
+		}
+		//Debug.Log (col);
+	}
+}
---
+++ Ass1/Assets/Scripts/WayPointHolder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WayPointHolder : MonoBehaviour {
+
+	public float score = 0;
+	public float timer;
+	public Text scoreText;
+	public Text Countdown;
+	// Use this for initialization
+	void Start () 
+	{
+		scoreText.text = score.ToString ();
+		timer = 30;
+		Countdown.text = timer.ToString ("##.##");
+	}
+
+	void Update()
+	{
+		timer -= Time.deltaTime;
+		Countdown.text = timer.ToString ("##.##");
+	}
+
+	// Update is called once per frame
+	public void UpdateScore () 
+	{
+		score += 100;
+		scoreText.text = score.ToString ();
+		timer += 10;
+		Countdown.text = timer.ToString ("##.##");
+	}
+}
